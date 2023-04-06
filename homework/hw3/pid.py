f34af9779cc36240a2df6f8f7ea11a33aaafa738
Nikolay Zherdev <nikolay.zherdev@evocargo.com>
2023-04-06 22:16:18 +0300

    ENH: cleanhw3, add eng/rus readme

---
+++ homework/hw3/pid.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+class PID:
+	
+	# TODO: Complete the PID class. You may add any additional desired functions
+
+	def __init__(self, Kp, Ki=0.0, Kd=0.0):
+		# TODO: Initialize PID coefficients (and errors, if needed)
+	
+	def UpdateError(self, cte):
+		# TODO: Update PID errors based on cte
+	
+	def TotalError(self):
+		# TODO: Calculate and return the total error
+		return 0.0
