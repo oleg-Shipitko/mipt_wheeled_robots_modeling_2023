bfc72fdfb7d0c5238e74d645abca9f463a44ca42
Nikolay Zherdev <nikolay.zherdev@evocargo.com>
2023-05-17 12:51:17 +0300

    FIX: instructions and python code

--- homework/hw3/pid.py
+++ homework/hw3/pid.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 
 class PID:
-	
 	# TODO: Complete the PID class. You may add any additional desired functions
 
-	def __init__(self, Kp, Ki=0.0, Kd=0.0):
-		# TODO: Initialize PID coefficients (and errors, if needed)
-	
-	def UpdateError(self, cte):
-		# TODO: Update PID errors based on cte
-	
-	def TotalError(self):
-		# TODO: Calculate and return the total error
-		return 0.0
+    def __init__(self, Kp, Ki=0.0, Kd=0.0):
+        # Initialize PID coefficients
+
+        # Initialize the current error and the previous error
+
+        # Initialize the integral part
+        pass
+
+    def UpdateError(self, cte):
+        # Update PID errors based on cte
+        pass
+
+    def TotalError(self):
+        # Calculate and return the total error
+        pass
